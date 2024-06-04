46c9031c9a63e382749630e5c9f55e1a63ae6b8a
anapaulacd <anapaulacoqueirodogado@gmail.com>
2024-06-04 08:19:21 -0300

    Adicionei o texto de boas vindas.

--- cadastro.php
+++ cadastro.php
@@ -7,11 +7,10 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.122.0">
-    <title>cadastro</title>
+    <title>Cadastro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
-
+    <link rel="stylesheet" href="cover.css">
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/cover/">
 
     
@@ -21,7 +20,7 @@
 <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
-      .bd-placeholder-img {
+      .bd-placeholder-IMG {
         font-size: 1.125rem;
         text-anchor: middle;
         -webkit-user-select: none;
@@ -159,21 +158,39 @@
 <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
   <header class="mb-auto">
     <div>
-      <h3 class="float-md-start mb-0">Cover</h3>
+      <h3 class="float-md-start mb-0"></h3>
       <nav class="nav nav-masthead justify-content-center float-md-end">
-        <a class="nav-link fw-bold py-1 px-0 " aria-current="page" href="pag.php">Home</a>
-        <a class="nav-link fw-bold py-1 px-0 active" href="cadastro.php">Cadastro</a>
-        <a class="nav-link fw-bold py-1 px-0" href="log.php">login</a>
+        <a class="nav-link fw-bold py-1 px-0x" aria-current="pag.php" href="pag.php">Home</a>
+        <a class="nav-link fw-bold py-1 px-0" href="cadastro.php">Cadastro</a>
+        <a class="nav-link fw-bold py-1 px-0 active" href="log.php">login</a>
       </nav>
     </div>
   </header>
 
   <main class="px-3">
-    <h1>Cover your page.</h1>
-    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-    <p class="lead">
-      <a href="#" class="btn btn-lg btn-light fw-bold border-white bg-white">Learn more</a>
-    </p>
+  <form action="" method="get">
+      <div class="col-3">
+        <label for="inputAddress" class="form-label">Nome</label>
+        <input type="text" class="form-control" id="nome" placeholder="">
+      </div><br>
+      
+      <div class="col-3">
+        <label for="inputAddress" class="form-label">Email</label>
+        <input type="text" class="form-control" id="email" placeholder="">
+      </div><br>
+      <div class="col-3">
+        <label for="inputAddress" class="form-label">Senha</label>
+        <input type="text" class="form-control" id="senha" placeholder="">
+      </div><br>
+      <div class="col-md-3">
+        <label for="inputAddress2" class="form-label">Confirmar Senha</label>
+        <input type="text" class="form-control" id="csenha">
+      </div><br>
+      <
+      <div class="col-3">
+        <button type="submit" class="btn btn-dark">Cadastrar</button>
+      </div><br>
+    </form>
   </main>
 
   <footer class="mt-auto text-white-50">
@@ -181,7 +198,6 @@
   </footer>
 </div>
 <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
-
 
     </body>
 </html>
--- pag.php
+++ pag.php
@@ -158,9 +158,9 @@
 <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
   <header class="mb-auto">
     <div>
-      <h3 class="float-md-start mb-0">Cover</h3>
+      <h3 class="float-md-start mb-0"></h3>
       <nav class="nav nav-masthead justify-content-center float-md-end">
-        <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="pag.php">Home</a>
+        <a class="nav-link fw-bold py-1 px-0 active" aria-current="pag.php" href="pag.php">Home</a>
         <a class="nav-link fw-bold py-1 px-0" href="cadastro.php">Cadastro</a>
         <a class="nav-link fw-bold py-1 px-0" href="log.php">login</a>
       </nav>
@@ -168,10 +168,10 @@
   </header>
 
   <main class="px-3">
-    <h1>Cover your page.</h1>
-    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+    <h1>seja Bem-vind@</h1>
+    <p class="lead"> Essa página é a página principal de um pequeno projeto criado para o ensino de acesso á base de dados utilizando PHP como linguagem de bach-end. Acesse o repositório do projeto no</p>
     <p class="lead">
-      <a href="#" class="btn btn-lg btn-light fw-bold border-white bg-white">Learn more</a>
+      <a href="#" class="btn btn-lg btn-light fw-bold border-white bg-white">github</a>
     </p>
   </main>
 
